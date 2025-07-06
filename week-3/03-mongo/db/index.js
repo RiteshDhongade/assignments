4e8374c416c6e4b8ab50c97c1758a92610c768e3
Your Name <you@example.com>
2025-07-06 22:55:54 +0530

    db update

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,18 +1,30 @@
+const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://admin:vJIJXT9puiYfYdvN@cluster0.g4mwkmd.mongodb.net/');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String // Schema definition here
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String,
+    parchasedcourse: [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Course'
+    }]// Schema definition here
 });
 
 const CourseSchema = new mongoose.Schema({
+    title: String,
+    descpription: String,
+    ImageLink: String,
+    Price: Number
+
     // Schema definition here
 });
 
